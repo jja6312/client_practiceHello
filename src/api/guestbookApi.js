9a6a326a77cb297640084dbccd4b27008d0468b3
JiAnJeong <jja6312@naver.com>
2025-03-06 14:15:09 +0900

    fix:publicIp fix(64.110.84.61

--- src/api/guestbookApi.js
+++ src/api/guestbookApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/guestbook",
+  baseURL: "http://64.110.84.61:8080/api/guestbook",
 });
 
 export const fetchMessages = () => api.get("");

3ec0799e559e24abee56841ac83259a514042ce3
JiAnJeong <jja6312@naver.com>
2025-03-06 14:15:09 +0900

    fix:https

--- src/api/guestbookApi.js
+++ src/api/guestbookApi.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/guestbook",
+  baseURL: "https://practicehello.store/api/guestbook",
 });
-
 export const fetchMessages = () => api.get("");
 export const postMessage = (data) => api.post("", data);
